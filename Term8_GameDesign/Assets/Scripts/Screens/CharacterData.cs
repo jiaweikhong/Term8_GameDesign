2b5d5d4626215b096030b21264d77cfb3f31362a
celine <celine110303@gmail.com>
2020-07-12 02:04:55 +0800

    Title and Character Selection Page templates

---
+++ Term8_GameDesign/Assets/Scripts/Screens/CharacterData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="CharacterData")]
+public class CharacterData : ScriptableObject
+{
+    [SerializeField] 
+    private string characterName;
+    [SerializeField] 
+    private Sprite sprite;
+    [SerializeField]
+    private string title;
+    [SerializeField]
+    private string backstory;
+    [SerializeField]
+    private Sprite primarySprite;
+    [SerializeField]
+    private string primaryName;
+    [SerializeField]
+    private Sprite secondarySprite;
+    [SerializeField]
+    private string secondaryName;
+
+    public string CharacterName
+    { get
+        { return characterName; }
+    }
+    public Sprite Sprite
+    { get
+        { return sprite; }
+    }
+    public string Title
+    { get
+        { return title; }
+    }
+    public string Backstory
+    { get
+        { return backstory; }
+    }
+    public Sprite PrimarySprite
+    { get
+        { return primarySprite; }
+    }
+    public string PrimaryName
+    { get
+        { return primaryName; }
+    }
+    public Sprite SecondarySprite
+    { get
+        { return secondarySprite; }
+    }
+    public string SecondaryName
+    { get
+        { return secondaryName; }
+    }
+}
---
+++ Term8_GameDesign/Assets/Scripts/Screens/CharacterSelect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    [SerializeField] 
+    private Text characterName;
+    [SerializeField] 
+    private Image sprite;
+    [SerializeField]
+    private Text title;
+    [SerializeField]
+    private Image primarySprite;
+    [SerializeField]
+    private Text primaryName;
+    [SerializeField]
+    private Image secondarySprite;
+    [SerializeField]
+    private Text secondaryName;
+
+    public void UpdateDisplayUI(CharacterData data)
+    {
+        characterName.text = data.CharacterName;
+        sprite.sprite = data.Sprite;
+        title.text = data.Title;
+        primarySprite.sprite = data.PrimarySprite;
+        primaryName.text = data.PrimaryName;
+        secondarySprite.sprite = data.SecondarySprite;
+        secondaryName.text = data.SecondaryName;
+    }
+}
