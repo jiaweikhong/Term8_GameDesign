f083101a1246ece36925d760ae659fbee246322f
gexty444 <34784796+gexty444@users.noreply.github.com>
2020-07-25 22:15:42 +0800

    [WIP] Added pause menu canvas, but do not work with inputs

---
+++ Term8_GameDesign/Assets/Scripts/Screens/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        // TODO: change the input method
+/*        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        } */       
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+    }
+
+    public void LoadControls()
+    {
+        Debug.Log("Loading Controls...");
+    }
+}
