c0ced6f39493eb4e9b9905680bafd7b3898e9f7c
pencilleaf <claire_tan@mymail.sutd.edu.sg>
2020-07-28 15:33:27 +0800

    Added prof animations, tweak attacks for balance

--- Term8_GameDesign/Assets/Prefabs/Attacks/ProfessorSecondary.cs
+++ Term8_GameDesign/Assets/Prefabs/Attacks/ProfessorSecondary.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class ProfessorSecondary : MonoBehaviour
 {
-    public float speed = 450f;
+    public float speed = 500f;
     public Animator potionAnimator;
     public int casterPlayerNum;
+    public Vector3 casterPlayerSpeed;
     void OnEnable()
     {
+        Vector2 playerVelocityX = Vector2.right * casterPlayerSpeed.x;
         this.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(transform.right.x, 1.2f) * speed);
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(transform.right.x, 1.25f) * speed + playerVelocityX);
     }
     void OnTriggerEnter2D(Collider2D other) {
         // Probably bad coding practice...
-        if (other.tag != "Damage" && other.name!="Dr. Professor")
+        if (other.tag != "Damage" && other.name!="Dr. Professor" && other.tag!="pickup")
         {
             Debug.Log("Potion hit something!"+ other.name + other.tag);
             potionAnimator.SetTrigger("Explode");
             this.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-            Invoke("Destroy", 2f);
+            transform.position = transform.position + new Vector3(0, 0.2f, 0);
+            Invoke("Destroy", 3f);
         }
     }
     void Destroy()
