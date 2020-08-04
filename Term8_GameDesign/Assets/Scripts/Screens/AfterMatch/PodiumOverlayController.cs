42b623c502f3b053d4dc5ece3ede1c156d486f6e
celine <celine110303@gmail.com>
2020-08-04 22:12:47 +0800

    Implemented podium overlay

---
+++ Term8_GameDesign/Assets/Scripts/Screens/AfterMatch/PodiumOverlayController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodiumOverlayController : MonoBehaviour
+{
+    public int playerNum;
+    public PlayerStats playerStats;
+    public PodiumOverlayUI podiumOverlayUI;
+    private ScreensTransitionManager screensTransitionManager;
+    private AfterMatchManager afterMatchManager;
+    private bool isReady = false;
+    void Start()
+    {
+        screensTransitionManager = FindObjectOfType<ScreensTransitionManager>();
+        afterMatchManager = FindObjectOfType<AfterMatchManager>();
+
+        afterMatchManager.OnUpdateRank += UpdateRank;
+    }
+
+    public void UpdateRank()
+    {
+        podiumOverlayUI.UpdatePlayer(playerStats);
+        podiumOverlayUI.UpdateRank(afterMatchManager.getPlayerRank(playerNum));
+    }
+
+    public void UpdateReady()
+    {
+        isReady = !isReady;
+        podiumOverlayUI.UpdateReady(isReady);
+        screensTransitionManager.ReadyPlayer(isReady);
+    }
+}
